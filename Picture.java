0903ecfec8bf954fe4c708d7e99b565b324eb753
David Garcia <igdrasilenllamas@gmail.com>
2015-10-09 11:05:14 +0200

    Eliminado sun2 y creado floor

--- Picture.java
+++ Picture.java
@@ -15,7 +15,7 @@ public class Picture
     private Square window;
     private Triangle roof;
     private Circle sun;
-    private Circle sun2;
+    private Square floor;
 
     /**
      * Constructor for objects of class Picture
@@ -56,12 +56,8 @@ public class Picture
         sun.changeSize(80);
         sun.makeVisible();
         
-        sun2 = new Circle();
-        sun2.changeColor("green");
-        sun2.moveHorizontal(0);
-        sun2.moveVertical(-40);
-        sun2.changeSize(80);
-        sun2.makeVisible();
+        floor = new Square();
+        
     }
 
     /**
